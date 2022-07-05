6ff455ee8e42403fef7da3508f197f280bf0c965
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-05 16:54:20 +0200

    modified GenerationType from Sequence to Indentity

--- src/main/java/com/acoustic/entity/AnnualGross.java
+++ src/main/java/com/acoustic/entity/AnnualGross.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 @Builder
 public class AnnualGross {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private BigDecimal annualGrossAmount;
